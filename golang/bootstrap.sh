d351a70e414de962c5418e508eb39f3d66f20894
nocotan <noconoco.lib@gmail.com>
2017-02-26 18:11:19 +0900

    golang

--- golang/bootstrap.sh
+++ golang/bootstrap.sh
@@ -1,5 +1,10 @@
+echo "apt-get update..."
 sudo apt-get update
+
+echo "install git..."
 sudo apt-get install git -y
+
+echo "install golang..."
 sudo apt-get install golang-go -y
 sudo apt-get install mercurial -y
 
@@ -10,3 +15,8 @@ go get golang.org/x/tools/cmd/godoc
 go get golang.org/x/tools/cmd/vet
 go get github.com/golang/lint/golint
 go get github.com/go-martini/martini
+
+echo "install postgresql..."
+sudo apt-get install -y postgresql libpq-dev
+cat /etc/passwd | grep postgres
+psql --version
