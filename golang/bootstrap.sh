e996abe8bfe213437148cac78575a3372cc82ab2
nocotan <noconoco.lib@gmail.com>
2017-02-26 17:43:56 +0900

    add golang

---
+++ golang/bootstrap.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install golang-go -y
+sudo apt-get install mercurial -y
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=/home/vagrant/dev
+
+go get golang.org/x/tools/cmd/godoc
+go get golang.org/x/tools/cmd/vet
+go get github.com/golang/lint/golint
+go get github.com/go-martini/martini
